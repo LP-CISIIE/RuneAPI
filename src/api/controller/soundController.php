5fcb5dfc75d26318a6a0fcba3854e8a2b354005d
haksho <adrien-richard@live.fr>
2016-02-11 17:48:45 +0100

    gestion erreur file get content

--- src/api/controller/soundController.php
+++ src/api/controller/soundController.php
@@ -6,10 +6,25 @@ class soundController
 {
     public static function volume($app, $vol)
     {
-    	header('Content-Type: application/json');
-    	header("HTTP/1.0 200 OK");
+    	//$app->response->headers->set('Content-Type', 'application/json');
+    	$array = array();
     	$file = file_get_contents($app->rootUri.'/command/?cmd=setvol%200'.$vol);
-    	$array = array('volume' => $vol );
+    	
+    	//si 400 ou 500
+    	if (!isset($http_response_header[0]) || preg_match("#[4-5][0-9]{2}#", $http_response_header[0])) {
+        	array_push($array,array('volume' => $vol,
+        							'Code'=> $http_response_header[0]
+
+        		) );
+    	} else {
+    	array_push($array,array('volume' => $vol,
+    							'code' => $http_response_header[0]
+    		) );
+
+    	}
+
+    	
     	echo json_encode($array);
+    	
     }
 }
