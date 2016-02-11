c61cd78c7311d86fd510cf30362675d48129bdd0
Mathis <mdupuy57@gmail.com>
2016-02-11 16:05:57 +0100

    tests

--- src/api/controller/playerController.php
+++ src/api/controller/playerController.php
@@ -7,11 +7,23 @@ class playerController
     public static function player($app, $action)
     {
         switch ($action) {
-            case 'play' : self::play($app); break;
-            case 'stop' : self::stop($app); break;
-            case 'next' : self::next($app); break;
-            case 'previous' : self::previous($app); break;
-            default : echo 'it works'; break;
+            case 'play' :
+                self::play($app);
+                break;
+            case 'pause' :
+                self::pause($app);
+            case 'stop' :
+                self::stop($app);
+                break;
+            case 'next' :
+                self::next($app);
+                break;
+            case 'previous' :
+                self::previous($app);
+                break;
+            default :
+                echo 'it works';
+                break;
         }
     }
 
@@ -21,18 +33,22 @@ class playerController
 
         // l'url du html à récupérer
         $html = file_get_contents($app->rootUri . '/command/?cmd=play');
-        echo $html.'yolo';
-
-        if($html == "OK "){
+        var_dump ($http_response_header[0]);
+        echo strstr($html, 'OK');
+//        echo($http_response_header[0]);
+//        echo strcmp("OK", $html);
+        if(strstr($html, 'OK') == "OK"){
+            $app->response->setStatus(200);
             echo json_encode(array(
                 "Object" => "play",
                 "message" => "Done"
             ));
         }else{
-            echo json_encode(array(
-                "Object" => "play",
-                "message" => "Error"
-            ));
+//            $app->response->setStatus(500);
+//            echo json_encode(array(
+//                "Object" => "play",
+//                "message" => "Error"
+//            ));
         }
     }
 
@@ -41,6 +57,13 @@ class playerController
         $app->response->headers->set('Content-Type', 'application/json');
         // l'url du html à récupérer
         $html = file_get_contents($app->rootUri . '/command/?cmd=stop');
+    }
+
+    public static function pause($app)
+    {
+        $app->response->headers->set('Content-Type', 'application/json');
+        // l'url du html à récupérer
+        $html = file_get_contents($app->rootUri . '/command/?cmd=pause');
     }
 
     public static function next($app)
