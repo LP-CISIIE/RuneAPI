f7269d2f1fab46465f01bfb7d7bdf56b1022c566
haksho <adrien-richard@live.fr>
2016-02-11 17:24:08 +0100

    header comme ca ?

--- src/api/controller/soundController.php
+++ src/api/controller/soundController.php
@@ -6,11 +6,10 @@ class soundController
 {
     public static function volume($app, $vol)
     {
+    	header('Content-Type: application/json');
+    	header("HTTP/1.0 200 OK");
     	$file = file_get_contents($app->rootUri.'/command/?cmd=setvol%200'.$vol);
-    	$array = array('volume' => $vol,
-    					"HTTP" => 200,
-               			"Object" => "volume",
-               			"message" => "Done");
+    	$array = array('volume' => $vol );
     	echo json_encode($array);
     }
 }
