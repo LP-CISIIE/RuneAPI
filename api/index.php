345890e31a68b0c3b9e1a6efd65c9dfb7356a122
Mathis <mdupuy57@gmail.com>
2016-02-11 15:20:08 +0100

    add control of the player

--- api/index.php
+++ api/index.php
@@ -2,11 +2,13 @@
 
 require '../vendor/autoload.php';
 use api\controller\settingsController;
+use api\controller\playerController;
 
 $app = new Slim\Slim(array(
     'view' => new \Slim\Views\Twig()
 ));
 $app->contentType('text/html; charset=utf-8');
+$app->rootUri = "http://192.168.1.14";
 
 $app->get('/', function() use ($app) {
 	echo "ça marche";
@@ -15,5 +17,9 @@ $app->get('/', function() use ($app) {
 $app->get('/settings', function() use ($app) {
     settingsController::settings($app);
 })->name('annoncesId');
+
+$app->get('/player/:action', function($action) use ($app) {
+    playerController::player($app, $action);
+})->name('player');
 
 $app->run();
