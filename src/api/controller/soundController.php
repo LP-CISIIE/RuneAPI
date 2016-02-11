48adabd06952b245d328d0c12682c38b86ecc3a8
haksho <adrien-richard@live.fr>
2016-02-11 17:19:51 +0100

    son avec retour json

--- src/api/controller/soundController.php
+++ src/api/controller/soundController.php
@@ -7,6 +7,10 @@ class soundController
     public static function volume($app, $vol)
     {
     	$file = file_get_contents($app->rootUri.'/command/?cmd=setvol%200'.$vol);
-    	
+    	$array = array('volume' => $vol,
+    					"HTTP" => 200,
+               			"Object" => "volume",
+               			"message" => "Done");
+    	echo json_encode($array);
     }
 }
