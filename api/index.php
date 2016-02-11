97d3153f07e830508013994b5c43610b3e505994
Mathis <mdupuy57@gmail.com>
2016-02-11 16:50:19 +0100

    manage error on player

--- api/index.php
+++ api/index.php
@@ -3,6 +3,7 @@
 require '../vendor/autoload.php';
 use api\controller\settingsController;
 use api\controller\playerController;
+use api\controller\playlistController;
 
 $app = new Slim\Slim(array(
     'view' => new \Slim\Views\Twig()
@@ -21,5 +22,9 @@ $app->get('/settings', function() use ($app) {
 $app->get('/player/:action', function($action) use ($app) {
     playerController::player($app, $action);
 })->name('player');
+
+$app->get('/playlist/:action', function($action) use ($app) {
+    playlistController::playlist($app, $action);
+})->name('playlist');
 
 $app->run();
