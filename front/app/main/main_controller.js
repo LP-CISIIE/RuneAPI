711f291db62cbff58454a1d30f3d4561f35d9000
Mathis <mdupuy57@gmail.com>
2016-03-10 16:10:37 +0100

    fix switch between screens

--- front/app/main/main_controller.js
+++ front/app/main/main_controller.js
@@ -3,17 +3,9 @@
  */
 rune.controller('MainController',
     ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
-        $scope.show = 1;
-        $scope.show_player = false;
+        $scope.show_player = true;
         $scope.show_manager = false;
         $scope.show_settings = false;
-
-        switch($scope.show)
-        {
-            case 1 : $scope.show_player = true; $scope.show_manager = false; $scope.show_settings = false; break;
-            case 2 : $scope.show_player = false; $scope.show_manager = true; $scope.show_settings = false; break;
-            case 3 : $scope.show_player = true; $scope.show_manager = false; $scope.show_settings = true; break;
-        }
 
         console.log($rootScope.runes);
         $scope.$watch('$rootScope.rune', function () {
