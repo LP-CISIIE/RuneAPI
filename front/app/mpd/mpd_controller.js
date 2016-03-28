c4b7823acdcf8527a7458737570fd48ccbcbcb29
schleft <kevin.legougne@gmail.com>
2016-03-28 19:17:37 +0200

    mpd modif js

--- front/app/mpd/mpd_controller.js
+++ front/app/mpd/mpd_controller.js
@@ -3,7 +3,36 @@ rune.controller('MpdController',['$scope', '$http', '$rootScope', 'runeCurrent',
 	$http.get('http://localhost/RuneAPI/api/index.php/mpd')
                 .then(function(data){
                     $scope.dataMpd = data.data.message;
-                    console.log($scope.dataMpd);
     });
+
+    $scope.validate = function () {
+    	console.log("test");
+
+		$('#modal2').openModal();
+		$http({
+ 			method: 'POST',
+ 			url: 'http://localhost/RuneAPI/api/index.php/mpd',
+ 			headers: {
+   			'Content-Type': undefined
+ 			},
+ 			data: {
+				"mixer_type": document.getElementById('VolumeControle').value,
+				"user": document.getElementById('DaemonUser').value,
+				"log_level" : document.getElementById('LogLevel').value,
+				"state_file" : document.getElementById('StateFile').value,
+				"ffmpeg" : document.getElementById('FFmpeg').value,
+				"gapless_mp3_playback" : document.getElementById('Gapless').value,
+				"dsd_usb" : document.getElementById('DSDSupport').value,
+				"volume_normalization" : document.getElementById('VolumeNormalization').value,
+				"audio_buffer_size" : document.getElementById('AudioBufferSuze').value,
+				"buffer_before_play" : document.getElementById('Buffer').value,
+				"auto_update" : document.getElementById('AutoUpdate').value
+			}
+		}).then(function(data){
+			console.log(data);			
+			$('#modal2').closeModal();
+			$('#modal1').openModal();
+		});
+    }
 
 }]);
