9a3794042e1c320ed00c9738cce6e37dec7c17f5
haksho <adrien-richard@live.fr>
2016-03-28 12:55:30 +0200

    credits

--- front/app/credits/credits_controller.js
+++ front/app/credits/credits_controller.js
@@ -1,11 +1,12 @@
 rune.controller('CreditsController',['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
 
 	$scope.team = function() {
-            $http.get($rootScope.root + '/credits')
+            $http.get('http://localhost/RuneAPI/api/index.php/credits')
                 .then(function(data){
                     $scope.datas=JSON.parse(data.data.credits);
+                    
                     console.log(data.data.credits);
                 })
         };
-	//$scope.team();
+	$scope.team();
 }]);
