806ffaaf025ea84e5791b08a2a78fb1056ad5b00
Mathis <mdupuy57@gmail.com>
2016-03-28 17:55:07 +0200

    add communication test on rune add

--- front/app/rune/rune_directive.js
+++ front/app/rune/rune_directive.js
@@ -1,20 +1,33 @@
 /**
  * Created by LocoMan on 26/02/2016.
  */
-rune.directive('runemanager', ['$rootScope', 'Rune', 'runeCurrent', function($rootScope, Rune, runeCurrent) {
+rune.directive('runemanager', ['$rootScope', 'Rune', 'runeCurrent', '$http', function($rootScope, Rune, runeCurrent, $http) {
     // pour appeler les templates
     return {
         templateUrl: 'template/rune-manager.html',
         link: function(scope) {
             scope.add_rune = function(addRune){
                 console.log(addRune);
-                scope.runes.push(new Rune(addRune));
-                scope.runePush();
+                $http.get('http://' + addRune.ip).then(function(response){
+                    console.log(response);
+                    if(response.data == "ça marche"){
+                        console.log('rune added');
+                        scope.runes.push(new Rune(addRune));
+                        scope.runePush();
 
-                $rootScope.runes = scope.runes;
+                        $rootScope.runes = scope.runes;
 
-                scope.addRune.label = "";
-                scope.addRune.ip = "";
+                        scope.addRune.label = "";
+                        scope.addRune.ip = "";
+                    }
+                    else{
+                        alert('Communication impossible avec l\'API. Vérifiez l\'IP et le port 83');
+                    }
+                }, function(error){
+                    alert('Communication impossible avec le rune. Vérifiez l\'IP et le port 83');
+                    console.log(error);
+                });
+
             };
             scope.delete_rune = function(delRune){
                 scope.runeDelete(delRune);
